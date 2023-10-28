487e3034ab227fafd593d1ebf1f5bab730b7f71a
yakintech <cagatayyildiz87@gmail.com>
2023-10-28 10:41:36 +0300

    Initial Commit

---
+++ MoneyTrackerMobileApp/MoneyTrackerMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTrackerMobileAppApp.swift
+//  MoneyTrackerMobileApp
+//
+//  Created by Çağatay Yıldız on 28.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTrackerMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
